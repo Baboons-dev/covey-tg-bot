0bfb943162a1aaa7fa4e1d76c3e9fc08b1c5fb8e
Waqas Ali <sawaldherwa@gmail.com>
2024-11-13 16:25:59 +0500

    Add some more stuff

---
+++ next-auth.d.ts
@@ -0,0 +1,20 @@
+import { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      role?: string | null;
+    } & DefaultSession["user"];
+  }
+
+  interface User {
+    id: string;
+    role?: string | null;
+  }
+
+  interface JWT {
+    id: string;
+    role?: string | null;
+  }
+}
